65b7dff6e00434a3006ea19c54d4ac3cd652dc9f
edigonzales <edi.gonzales@gmail.com>
2021-09-17 17:00:43 +0200

    add dataproductresult pojo

---
+++ searchservice-server/src/main/java/ch/so/agi/DataproductResult.java
@@ -0,0 +1,49 @@
+package ch.so.agi;
+
+import java.util.List;
+
+public class DataproductResult {
+    private String dataproductId;
+    private String display;
+    private boolean destInfo;
+    private String stacktype;
+    private String type;
+    private List<DataproductResult> sublayers;
+    
+    public String getDataproductId() {
+        return dataproductId;
+    }
+    public void setDataproductId(String dataproductId) {
+        this.dataproductId = dataproductId;
+    }
+    public String getDisplay() {
+        return display;
+    }
+    public void setDisplay(String display) {
+        this.display = display;
+    }
+    public boolean isDestInfo() {
+        return destInfo;
+    }
+    public void setDestInfo(boolean destInfo) {
+        this.destInfo = destInfo;
+    }
+    public String getStacktype() {
+        return stacktype;
+    }
+    public void setStacktype(String stacktype) {
+        this.stacktype = stacktype;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public List<DataproductResult> getSublayers() {
+        return sublayers;
+    }
+    public void setSublayers(List<DataproductResult> sublayers) {
+        this.sublayers = sublayers;
+    }
+}
--- searchservice-server/src/main/java/ch/so/agi/SearchResult.java
+++ searchservice-server/src/main/java/ch/so/agi/SearchResult.java
@@ -1,5 +1,21 @@
 package ch.so.agi;
 
-public class SearchResult {
+import java.util.List;
 
+public class SearchResult {
+    List<FeatureResult> features;
+    List<DataproductResult> dataproducts;
+    
+    public List<FeatureResult> getFeatures() {
+        return features;
+    }
+    public void setFeatures(List<FeatureResult> features) {
+        this.features = features;
+    }
+    public List<DataproductResult> getDataproducts() {
+        return dataproducts;
+    }
+    public void setDataproducts(List<DataproductResult> dataproducts) {
+        this.dataproducts = dataproducts;
+    }
 }
